c5e4b30aeb1ecfee29c64d979e7731f07d4d3094
Tek <tek@randhome.io>
2017-11-28 14:32:16 -0500

    add search

---
+++ pe/plugins/search.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+import sys
+import json
+import hashlib
+import pefile
+import datetime
+from pe.plugins.base import Plugin
+
+class PluginSearch(Plugin):
+    name = "search"
+    description = "Search string in a PE file"
+
+    def add_arguments(self, parser):
+        parser.add_argument('STRING', help='a string')
+        self.parser = parser
+
+    def run(self, pe, args):
+        with open(args.PEFILE, 'rb') as f:
+            data = f.read()
+
+        pos = data.find(args.STRING.encode('utf-8'))
+        if pos == -1:
+            print("String not found...")
+            sys.exit(1)
+
+        print('Position in the file : 0x%x' % pos)
+
+        # Search position in the PE
+        # Check in sections first
+        for s in pe.sections:
+            if (pos >= s.PointerToRawData) and (pos <= s.PointerToRawData + s.SizeOfRawData):
+                vaddr = pe.OPTIONAL_HEADER.ImageBase + pos - s.PointerToRawData + s.VirtualAddress
+                print("In section %s at address 0x%x" % (s.Name, vaddr))
