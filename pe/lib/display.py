20bda8ef8a0d0458bfcea20ea4c44733dc6faae0
Tek <tek@randhome.io>
2019-11-12 00:17:26 +0100

    minor bug

--- pe/lib/display.py
+++ pe/lib/display.py
@@ -2,7 +2,7 @@ import hashlib
 
 def display_sections(pe):
     """Display information about the PE sections"""
-    print("%-10s %-9s %-9s %-9s %-9s %-8s %s" % ( "Name", "VirtSize", "VirtAddr", "RawSize", "RawAddr", "Entropy", "md5"))
+    print("%-10s %-9s %-9s %-9s %-9s %-8s %s" % ( "Name", "VirtSize", "VirtAddr", "RawAddr", "RawSize", "Entropy", "md5"))
     for section in pe.sections:
         name = section.Name.decode('utf-8', 'ignore').strip('\x00')
         m = hashlib.md5()
