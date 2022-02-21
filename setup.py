50190e6ac1aacad4fd624a126db6c27ea26491ed
tek <tek@randhome.io>
2022-02-22 00:15:00 +0100

    Bumps version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pecli',
-    version='0.1.3',
+    version='0.1.4',
     description='Another PE info tool',
     long_description=long_description,
     long_description_content_type="text/markdown",
